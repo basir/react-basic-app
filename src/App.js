b03ffb7bfb13c474c744de595a1a612c9af043fa
Basir <basir.jafarzadeh@gmail.com>
2021-05-01 13:34:19 +0430

    Video-04-Intro-To-JSX

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  function formatName(userName) {
+    return userName.toUpperCase();
+  }
+  function getGreeting(username) {
+    if (username) {
+      return <h1>Hello {formatName(username)}</h1>;
+    } else {
+      return <h1>Hello Stranger</h1>;
+    }
+  }
+  const name = 'John Smith';
+  const userImage = '/logo192.png';
+  const image = <img className="sample-class" src={userImage} alt="logo" />;
+  const element = (
+    <div>
+      <h1>Hello {name}</h1>
+      <p>Good to see you</p>
+      {image}
     </div>
   );
+
+  return element;
 }
 
 export default App;
